6b7362a422451c1fd458a19483ea9a442ddf899b
SPBONIFACE <sandybony15@hotmail.com>
2022-01-07 14:43:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 [theme]\n\
-primaryColor = ‘#84a3a7’\n\
-backgroundColor = ‘#EFEDE8’\n\
-secondaryBackgroundColor = ‘#fafafa’\n\
-textColor= ‘#424242’\n\
-font = ‘sans serif’\n\
+primaryColor = '#84a3a7'\n\
+backgroundColor = '#EFEDE8'\n\
+secondaryBackgroundColor = '#fafafa'\n\
+textColor= '#424242'\n\
+font = 'sans serif'\n\
 \n\
 " > ~/.streamlit/config.toml
