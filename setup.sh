27f2a1324e0bed9f4409f93894ba1cccfc813058
SPBONIFACE <sandybony15@hotmail.com>
2022-01-07 14:30:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]\n\
-primaryColor=’#020202’\n\
-backgroundColor=’#c4c3c3’\n\
-secondaryBackgroundColor=’#ebd316’\n\
-font = ‘sans serif’\n\
-\n\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
