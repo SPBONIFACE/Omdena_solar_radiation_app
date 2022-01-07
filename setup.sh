8dbc397d84dd5c0b31dca9a2d6b1d7958146ee13
SPBONIFACE <sandybony15@hotmail.com>
2022-01-07 14:36:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]\n\
+primaryColor = ‘#84a3a7’\n\
+backgroundColor = ‘#EFEDE8’\n\
+secondaryBackgroundColor = ‘#fafafa’\n\
+textColor= ‘#424242’\n\
+font = ‘sans serif’\n\
+\n\
 " > ~/.streamlit/config.toml
