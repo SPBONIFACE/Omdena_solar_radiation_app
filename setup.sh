60edc9e4305455fc24e864dd1f2c73c09a810707
SPBONIFACE <sandybony15@hotmail.com>
2022-01-07 11:36:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"sandybony15@hotmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "[theme]
-primaryColor='#b31b1b'
-backgroundColor=’#efefef’
-secondaryBackgroundColor=’#eaeaea’
-textColor = '#333333'
-font = ‘sans serif’
-
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
