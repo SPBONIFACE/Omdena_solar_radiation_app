619509577ae969e9e7bc83e4bd47ac25e9fed8a6
SPBONIFACE <sandybony15@hotmail.com>
2022-01-07 14:26:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 mkdir -p ~/.streamlit/
+
 echo "\
-[theme]
-primaryColor=’#020202’
-backgroundColor=’#c4c3c3’
-secondaryBackgroundColor=’#ebd316’
-font = ‘sans serif’
+[theme]\n\
+primaryColor=’#020202’\n\
+backgroundColor=’#c4c3c3’\n\
+secondaryBackgroundColor=’#ebd316’\n\
+font = ‘sans serif’\n\
 \n\
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
